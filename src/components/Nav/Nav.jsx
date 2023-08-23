f4b2db76a936966f5e0cbf671f20f362eda1d65c
Railrik <benjamin@ligny.pro>
2023-08-23 23:42:35 +0200

    :sparkles: mise en place du bouton menu et mécanisme GSAP

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './Nav.scss';
+import { gsap } from 'gsap';
+
+const Nav = () => {
+    const [toggle, setToggle] = useState(false);
+    const menuButtonRef = useRef(null);
+    const navMainRef = useRef(null);
+
+    // Gère le clic sur le bouton de menu
+    const handleOnClick = () => {
+        setToggle(prevToggle => !prevToggle);
+    };
+
+    // Animation lorsque la souris entre dans le bouton de menu
+    const handleMouseEnter = () => {
+        gsap.to(menuButtonRef.current, { scale: 1.2, duration: 0.3, ease: 'power1.out' });
+    };
+
+    // Animation lorsque la souris quitte le bouton de menu
+    const handleMouseLeave = () => {
+        gsap.to(menuButtonRef.current, { scale: 1, duration: 0.3, ease: 'power1.out' });
+    };
+
+    useEffect(() => {
+        // Initialise la position des liens
+        gsap.set('.link', { y: 30 });
+
+        // Crée l'animation pour le menu ouvert
+        const menuAnimation = gsap.timeline({ paused: true });
+
+        // Crée l'animation pour le menu fermé (reversed)
+        const menuAnimationBack = gsap.timeline({ paused: true, reversed: true });
+
+        // Récupère la référence à l'élément nav
+        const navMain = navMainRef.current;
+
+        if (toggle) {
+            // Animation lorsque le menu est ouvert
+            gsap.to(menuButtonRef.current, { rotation: 360, duration: 0.3, ease: 'power2.out' });
+            menuAnimation
+                .to(navMain, { duration: 0.5, opacity: 1, width: '100%', clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", ease: "power2.inOut", x: 0, y: 0, z: 0 })
+                .to('.link', { duration: 0.5, autoAlpha: 1, y: 0, stagger: .2, ease: "power3.out" });
+            menuAnimation.play();
+        } else {
+            // Animation lorsque le menu est fermé
+            gsap.to(menuButtonRef.current, { rotation: 0, duration: 0.3, ease: 'power2.out' });
+            menuAnimationBack
+                .to('.link', { duration: 0.5, autoAlpha: 0, y: 50, stagger: .2, ease: "power3.out" })
+                .to(navMain, { duration: 0.5, width: 0, clipPath: "polygon(0 0, -100% 0, 100% 100%, 0 100%)", ease: "power4.in", x: 0, y: 0, z: 0 });
+            menuAnimationBack.play();
+        }
+    }, [toggle]);
+
+    return (
+        <>
+            <nav id="nav-main" className="skew" ref={navMainRef}>
+                <ul>
+                    <li className="link"><a href="#my-work">Work</a></li>
+                    <li className="link"><a href="#my-skills">Skills</a></li>
+                    <li className="link"><a href="#about-me">Qualities</a></li>
+                    <li className="link"><a href="#my-hobbies">Hobbies</a></li>
+                    <li className="link"><a href="#contact">contact</a></li>
+                </ul>
+            </nav>
+            <button id="menu-button" className="menu-button" onClick={handleOnClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={menuButtonRef}>
+                {toggle ? <i className="fa-sharp fa-solid fa-xmark" /> : <i className="fa-solid fa-plus" />}
+            </button>
+        </>
+    );
+};
+
+export default Nav;
--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import "./Wrapper.scss";
+import './Wrapper.scss';
 import Progress from '../Progress/Progress';
 import Logo from '../Logo/Logo';
+import Nav from '../Nav/Nav';
 
 const Wrapper = () => {
     return (
         <div className="wrapper">
             <Progress />
             <Logo />
+            <Nav />
         </div>
-
     );
 };
 
