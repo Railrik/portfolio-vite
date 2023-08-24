783835dc2e12b4804fa6b023d6782235e7147083
Railrik <benjamin@ligny.pro>
2023-08-24 19:27:58 +0200

    :hammer: mise en place du début du code de changement de couleur Logo et Background en fonction des sections lors du scroll

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -11,21 +11,27 @@ import Header from '../Header/Header';
 import Lang from '../Lang/Lang';
 import Work from '../Pages/Work/Work';
 
+import logoImg from '../../assets/img/logo.png';
+import logoWhiteImg from '../../assets/img/logo-white.png';
+
 const Wrapper = () => {
     const [scrub, setScrub] = useState(1.2)
+
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
         setupSectionsAnimations();
+        setupSectionsChangeBgColor();
     }, []);
 
+    // Configuration des animations de défilement des sections
     const setupSectionsAnimations = () => {
         const sections = document.querySelectorAll("section");
-        const scrub = 1.2;
 
         sections.forEach((section, index) => {
             const wrapperContent = section.querySelector(".wrapper-content");
+            const isEven = index % 2 === 0;
+
             if (wrapperContent) {
-                const isEven = index % 2 === 0;
                 const [x, xEnd] = isEven
                     ? [(wrapperContent.scrollWidth) * -1, 0]
                     : ["100%", (wrapperContent.scrollWidth - section.offsetWidth) * -1];
@@ -41,6 +47,63 @@ const Wrapper = () => {
         });
     };
 
+    // Configuration du changement de couleur de fond en fonction des sections
+    const setupSectionsChangeBgColor = () => {
+        const sections = document.querySelectorAll(".section");
+        const logoImage = document.getElementById("logo");
+
+        sections.forEach((section, index) => {
+            const isEven = index % 2 === 0;
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top center",
+                    end: "bottom center",
+                    onEnter: () => handleSectionEnter(tl, isEven),
+                    onEnterBack: () => handleSectionEnterBack(tl, isEven),
+                    onLeave: () => handleSectionLeave(tl, isEven),
+                    onLeaveBack: () => handleSectionLeaveBack(tl, isEven),
+                },
+            });
+
+            const handleSectionEnter = (tl, isEven) => {
+                const targetBgColor = isEven ? '#212121' : '#beccd4';
+                const targetLogoSrc = isEven ? logoWhiteImg : logoImg;
+
+                logoImage.src = targetLogoSrc;
+
+                tl.to("body", { backgroundColor: targetBgColor });
+            };
+
+            const handleSectionEnterBack = (tl, isEven) => {
+                const targetBgColor = isEven ? '#212121' : '#beccd4';
+                const targetLogoSrc = isEven ? logoWhiteImg : logoImg;
+
+                logoImage.src = targetLogoSrc;
+
+                tl.to("body", { backgroundColor: targetBgColor });
+            };
+
+            const handleSectionLeave = (tl, isEven) => {
+                const targetBgColor = isEven ? '#beccd4' : '#212121';
+                const targetLogoSrc = isEven ? logoImg : logoWhiteImg;
+
+                logoImage.src = targetLogoSrc;
+
+                tl.to("body", { backgroundColor: targetBgColor });
+            };
+
+            const handleSectionLeaveBack = (tl, isEven) => {
+                const targetBgColor = isEven ? '#beccd4' : '#212121';
+                const targetLogoSrc = isEven ? logoImg : logoWhiteImg;
+
+                logoImage.src = targetLogoSrc;
+
+                tl.to("body", { backgroundColor: targetBgColor });
+            };
+
+        });
+    };
     return (
         <div className="wrapper">
             <Progress />
