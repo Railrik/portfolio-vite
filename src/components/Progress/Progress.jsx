bcd7b82e0e9d18bebbe1cd242e172c89d55eec12
Railrik <benjamin@ligny.pro>
2023-08-27 00:55:49 +0200

    :hammer: mise à jour de l'effet progress bar lors du scroll

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import './Progress.scss';
-const Progress = () => {
+
+const Progress = ({ scrub }) => {
+    const [animationDuration, setAnimationDuration] = useState(0);
+    const [isBottom, setIsBottom] = useState(false);
+
+    useEffect(() => {
+        setAnimationDuration(document.documentElement.scrollHeight - window.innerHeight);
+    }, []);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        console.log(isBottom);
+        gsap
+            .timeline({
+                scrollTrigger: {
+                    trigger: ".wrapper",
+                    start: "top top",
+                    end: "bottom bottom",
+                    scrub: scrub,
+                    onEnterBack: () => setIsBottom(false),
+                    onLeave: () => setIsBottom(true),
+                    markers: true
+                },
+            })
+            .fromTo(
+                ".progress-info-fill",
+                { backgroundSize: "0% 100%" },
+                { backgroundSize: "100% 100%", duration: animationDuration },
+                0
+            )
+            .fromTo(
+                ".progress-info-fill-2",
+                { backgroundSize: "0% 100%" },
+                { backgroundSize: "0% 100%", duration: animationDuration },
+                0
+            );
+
+        if (isBottom) {
+            gsap.to(["#scroll-text-1", "#scroll-text-2"], { opacity: 0, duration: 0.5 });
+        } else {
+            gsap.to(["#scroll-text-1", "#scroll-text-2"], { opacity: 1, duration: 0.5 });
+        }
+
+    }, [animationDuration, isBottom]);
+
     return (
         <div className="progress-info">
-            <div className="progress-info-fill" id="scroll-text-1">Scroll</div>
-            <div className="progress-info-fill-2" id="scroll-text-2">Scroll</div>
+            <div className="progress-info-fill" id="scroll-text-1">___</div>
+            <div className="progress-info-fill-2" id="scroll-text-2">___</div>
         </div>
     );
 };
