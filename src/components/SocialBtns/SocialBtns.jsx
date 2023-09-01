8571b5a6f5b8e8f9ac7d9f188263e36b013c1360
Railrik <benjamin@ligny.pro>
2023-09-01 12:27:07 +0200

    :bento: Ajout de mon CV et bouton pour le telecharger

--- src/components/SocialBtns/SocialBtns.jsx
+++ src/components/SocialBtns/SocialBtns.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './SocialBtns.scss';
-
+import cv from '../../assets/pdf/cv.pdf'
 const SocialBtns = () => {
     return (
-        < div className='social'>
-            <a href="https://github.com/Railrik/portfolio-vite" target="_blank" id="github-icon" aria-label="Projet GitHub de mon portfolio (Benjamin Ligny)"><i className="fa-brands fa-github" /></a>
-            <a href="https://www.linkedin.com/in/benjamin-ligny/" target="_blank" id="linkedin-icon" aria-label="Ma page Linkedin"><i className="fa-brands fa-linkedin-in" /></a>
-            <a href="mailto:benjamin@ligny.pro" target="_blank" id="email-icon" aria-label="Contactez moi par E-mail"><i className="fa-solid fa-at" /></a>
+        <div className='social'>
+            <div className='social-column'>
+                <a href="https://github.com/Railrik/portfolio-vite" target="_blank" id="github-icon" aria-label="Projet GitHub de mon portfolio (Benjamin Ligny)"><i className="fa-brands fa-github" /></a>
+                <a href="https://www.linkedin.com/in/benjamin-ligny/" target="_blank" id="linkedin-icon" aria-label="Ma page Linkedin"><i className="fa-brands fa-linkedin-in" /></a>
+                <a href="mailto:benjamin@ligny.pro" target="_blank" id="email-icon" aria-label="Contactez moi par E-mail"><i className="fa-solid fa-at" /></a>
+            </div>
+            <div className='social-row'>
+                <a href={cv} target="_blank" id="pdf-icon" aria-label="Télécharger le CV de Benjamin Ligny"><i className="fa-solid fa-file-pdf"></i></a>
+            </div>
         </div>
+
     );
 };
 
